ce640aee5937c33805e4b1b5b75346ee363ea382
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-10-22 10:41:51 +0200

    fix FluxK#bracketCase test

--- modules/effects/arrow-effects-reactor-instances/src/test/kotlin/arrow/effects/FluxKTests.kt
+++ modules/effects/arrow-effects-reactor-instances/src/test/kotlin/arrow/effects/FluxKTests.kt
@@ -4,6 +4,7 @@ import arrow.effects.typeclasses.ExitCase
 import arrow.test.UnitSpec
 import arrow.typeclasses.Eq
 import io.kotlintest.KTestJUnitRunner
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.startsWith
 import org.hamcrest.MatcherAssert.assertThat
@@ -125,8 +126,9 @@ class FluxKTest : UnitSpec() {
 
       test
         .thenAwait(Duration.ofSeconds(5))
+        .then { assertThat(ec, `is`(ExitCase.Cancelled as ExitCase<Throwable>)) }
         .thenCancel()
-        .verifyThenAssertThat()
+        .verify()
     }
   }
 }
